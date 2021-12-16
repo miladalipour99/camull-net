c9bc0c6c3fda4ab887a371462d21cfe9d840c684
miladalipour99 <60439091+miladalipour99@users.noreply.github.com>
2021-12-16 02:04:55 -0800

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -58,10 +58,10 @@ def evaluate_model(device_in, uuid, ld_helper):
     fold = 0
 
     srch_path = "/content/weights/{}/".format(task_str) + uuid + "/*"
+    print(srch_path)
     for path in glob.glob(srch_path):
 
         print("Evaluating fold: ", fold + 1)
-        print(srch_path)
         model   = load_cam_model(path)
         model.to(device)
         test_dl = ld_helper.get_test_dl(fold)
