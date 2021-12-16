f7b5a21e7707f575965383b860ae8732a5045656
miladalipour99 <60439091+miladalipour99@users.noreply.github.com>
2021-12-16 00:56:25 -0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -134,7 +134,7 @@ def main():
     #NC v AD
     ld_helper = LoaderHelper(task=Task.NC_v_AD)
     model_uuid = train_camull(ld_helper, epochs=1)
-    evaluate_model(DEVICE, "c51bf83c4455416e8bc8b1ebbc8b75ca", ld_helper)
+    evaluate_model(DEVICE, uuid, ld_helper)
 
     #transfer learning for pMCI v sMCI
     #ld_helper.change_task(Task.sMCI_v_pMCI)
